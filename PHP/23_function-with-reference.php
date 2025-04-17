f268c5861cdd83c792c181c322c461388e52922d
anikmondol <anikmondol558363@gmail.com>
2025-04-18 00:06:42 +0600

    php video-30 done.

---
+++ PHP/23_function-with-reference.php
@@ -0,0 +1,41 @@
+<?php
+
+// function testing(&$string){
+    
+//     $string .= " and something extra";
+
+// }
+
+
+// $str = "This a string";
+
+
+// testing($str);
+
+// echo $str;
+
+
+function first($num){
+
+    $num += 5;
+
+}
+
+
+function second(&$num) {
+    $num += 5;
+
+}
+
+
+$number = 10;
+
+first($number);
+echo "Original value is $number <br>";
+
+
+second($number);
+echo "Original value is $number <br>";
+
+
+?>
---
+++ PHP/24_function-with-variable.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// function wow($name) {
+//     echo "Hello $name";
+// }
+
+
+
+// $func = "wow";
+
+// $func("Yahoo Baba");
+
+
+$sayHello = function($name) {
+    echo "hello $name";
+};
+
+$sayHello("anik");
+
+?>
