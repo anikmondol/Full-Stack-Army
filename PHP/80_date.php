aba6ccfbe873374bb55862b5cde2a8b4eec0d4b7
anikmondol <anikmondol558363@gmail.com>
2025-04-25 21:07:03 +0600

    video number-87 done

---
+++ PHP/80_date.php
@@ -0,0 +1,24 @@
+<?php
+
+
+echo "Today is : " . date("d") . "<br>";
+echo "Today is : " . date("j") . "<br>";
+echo "Today is : " . date("jS") . "<br>";
+echo "Month is : " . date("F") . "<br>";
+echo "Month is : " . date("m") . "<br>";
+echo "Month is : " . date("n") . "<br>";
+echo "Year is : " . date("Y") . "<br>";
+echo "Year is : " . date("y") . "<br>";
+echo "Full Date is : " . date("d/m/Y") . "<br>";
+echo "Week Day is : " . date("D") . "<br>";
+echo "Week Day is : " . date("N") . "<br>";
+echo "Year Day Number is : " . date("z") . "<br>";
+echo "Day Of the Month: " . date("t") . "<br>";
+echo "Is this is leap year: " . date("L") . "<br>";
+
+
+
+
+
+
+?>
---
+++ PHP/81_time.php
@@ -0,0 +1,22 @@
+<?php
+
+date_default_timezone_set("Asia/Dhaka");
+
+
+echo "Hour is " . date("h") . "<br>";
+echo "Hour is " . date("H") . "<br>";
+echo "Hour is " . date("g") . "<br>";
+echo "Hour is " . date("G") . "<br>";
+
+
+echo "Minutes is " . date("i") . "<br>";
+echo "Seconds is " . date("s") . "<br>";
+
+
+echo "Meridiem is " . date("A") . "<br>";
+
+
+echo "Full Time is " . date("d/m/Y,h:i:sa") . "<br>";
+
+
+?>
